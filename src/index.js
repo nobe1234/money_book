2c258e0214cc59f56b44fb35a4d4517775c22af1
nobe1234 <sohei.nobe@rakus.co.jp>
2019-08-22 15:12:02 +0900

    小遣い帳の大枠作成

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MoneyBook = () => {
+    return (
+        <div>
+            <h1>小遣い帳</h1>
+            <table className="book">
+                <thead>
+                    <tr><th>日付</th><th>項目</th><th>入金</th><th>出金</th></tr>
+                </thead>
+                <tbody>
+                    <tr><td>1/1</td><td>お年玉</td><td>10000</td><td></td></tr>
+                    <tr><td>1/3</td><td>ケーキ</td><td></td><td>500</td></tr>
+                    <tr><td>2/1</td><td>小遣い</td><td>3000</td><td></td></tr>
+                    <tr><td>2/5</td><td>漫画</td><td></td><td>600</td></tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+
+ReactDOM.render(<MoneyBook />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
